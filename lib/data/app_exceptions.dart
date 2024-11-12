34dee67af97810973cb6b47b2ecdbd8c70ec8027
M Saad <saadofficial0999@gmail.com>
2024-11-12 22:12:07 +0500

    handle api response complete

--- lib/data/app_exceptions.dart
+++ lib/data/app_exceptions.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_typing_uninitialized_variables
+// ignore_for_file: prefer_typing_uninitialized_variables, annotate_overrides
 
 class AppExceptions implements Exception {
   final _message;
---
+++ lib/data/response/api_response.dart
@@ -0,0 +1,23 @@
+import 'package:mvvm/data/response/status.dart';
+
+class ApiResponse<T> {
+  Status? status;
+  T? data;
+  String? message;
+
+  ApiResponse(this.status, this.data, this.message);
+
+  ApiResponse.loading() {
+    this.status = Status.LOADING;
+  } //: status = Status.LOADING;
+  ApiResponse.completed() {
+    this.status = Status.COMPLETED;
+  } // : status = Status.COMPLETED;
+  ApiResponse.error() {
+    this.status = Status.ERROR;
+  } // : status = Status.ERROR;
+
+  String toString() {
+    return 'Status: $status \n Message : $message \n Data : $data';
+  }
+}
